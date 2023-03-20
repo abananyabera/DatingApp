8cdd59ae38d2c798b8c40a75aa9ace30d1254a6d
Bera <anabera@deloitte.com>
2023-03-20 15:23:52 +0530

    End of Section-2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Ananya");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Rahul");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Sayak");
